245d61dac20e0255abfd6e4bf431a72836887e47
flb <f.lb@gmx.com>
2021-03-22 01:00:28 +0100

    Added split json version files

--- versions.py
+++ versions.py
@@ -1,6 +1,7 @@
 import json
 import cbor2
 import requests
+import os
 
 url = "https://raw.githubusercontent.com/blawar/titledb/master/cnmts.json"
 data = json.loads(requests.get(url).text.upper())
@@ -33,3 +34,21 @@ if(change):
         json_out = json.dumps(out)
         cbor2.dump(json_out, cbor_file)
     print("Updated " + path)
+
+dir_path = "versions/"
+if not(os.path.exists(dir_path)):
+    os.mkdir(dir_path)
+
+for tid in out:
+    path = dir_path + tid + ".json"
+    change = False
+    if(os.path.exists(path)):
+        with open(path, 'r') as json_file:
+            old = json.load(json_file)
+        if(json.dumps(old) != json.dumps(tid)):
+            change = True
+    else:
+        change = True
+    if(change):
+        with open(path, 'w') as json_file:
+            json.dump(out[tid], json_file, indent = 4)
