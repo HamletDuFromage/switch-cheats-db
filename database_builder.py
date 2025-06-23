d417b22a8f26d91cfec2fe925895c4991589bbf3
HamletDuFromage <61667930+HamletDuFromage@users.noreply.github.com>
2025-06-23 10:29:39 +0200

    fix workflow (untested)

--- database_builder.py
+++ database_builder.py
@@ -42,7 +42,8 @@ class GbatempCheatsInfo:
     def fetch_gbatemp_version(self):
         page = self.scraper.get(f"{self.page_url}/updates")
         soup = BeautifulSoup(page.content, "html.parser")
-        version = datetime.fromisoformat(soup.find("div", {"class": "block-container"}).find("time").get("datetime"))
+        dates = soup.find_all("time", {"class": "u-dt"}).get("datetime")
+        version = max([datetime.fromisoformat(date) for date in dates])
         return version.date()
 
     def has_new_cheats(self, database_version):
