a966044578aa06e1979ab374b8416797261a5bad
Reverse Telnet <56200159+ReverseTelnet@users.noreply.github.com>
2025-12-28 00:34:20 +0000

    Add exception catch when creating Title-ID directory

--- database_builder.py
+++ database_builder.py
@@ -110,7 +110,10 @@ class ArchiveWorker():
             titles_path.mkdir(parents=True)
         for tid in cheats_path.iterdir():
             tid_path = titles_path.joinpath(tid.stem)
-            tid_path.mkdir()
+            try:
+                tid_path.mkdir()
+            except FileExistsError:
+                continue
             with open(tid, "r") as cheats_file:
                 cheats_dict = json.load(cheats_file)
             for key, value in cheats_dict.items():
